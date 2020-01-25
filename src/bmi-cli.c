459f454b8e9f909873b33561e5c23a2d03e94a14
Kader DJEHAF <abdelkaderdjehaf@gmail.com>
2020-01-25 14:34:49 +0100

    Working basic

--- src/bmi-cli.c
+++ src/bmi-cli.c
@@ -1,55 +1,70 @@
 /* bmi-cli.c */
-
 #include <stdio.h>
+#include <stdlib.h>
+
 #include "bmi.h"
 
-#define SCANFERR "ERROR: scanf invalid input."
+#define SCANFERR "ERROR: invalid input."
+
+/*
+#define DUMP(varname)                                                         \
+  fprintf (stderr, "--DEBUG: %s = %x\n", #varname, varname);
+*/
 
 typedef int bool_t;
 
-int main()
+int
+main ()
 {
-  bool_t metrics = 0;
-  // short age = 0;
-  short weight = 0;
-  short height = 0;
 
-  // char *buffer = NULL;
+  bool_t metrics = 0;
+  int choice = 0;
+  // -- short *age_ = 0;
+
+  float weight = 0;
+  float height = 0;
+
+// char *buffer = NULL;
 START:
-  printf("Do you want use the Imperial or the Metric system to input data?\n"
-         "1. Imperial English\n"
-         "2. Metrics [Default]\n>");
-  if (scanf("%d", &metrics) != 1)
-    fprintf(stderr, SCANFERR);
+  printf ("Do you want use the Imperial or the Metric system to input data?\n"
+          "1. Imperial English\n"
+          "2. Metrics [Default]\n>");
+  if (scanf ("%d", &choice) != 1)
+    fprintf (stderr, SCANFERR);
+
+  metrics = choice;
 
   /* ask user for height */
-  printf("How tall are you?\n");
-  if (scanf("%hi", &height) != 1)
-    fprintf(stderr, SCANFERR);
+  printf ("How tall are you?\n");
+  if (scanf ("%f", &height) != 1)
+    fprintf (stderr, SCANFERR);
 
   /* ask user for their weight */
-  printf("How much do you weigh?\n");
-  if (scanf("%hi", &weight) != 1)
-    fprintf(stderr, SCANFERR);
+  printf ("How much do you weigh?\n");
+  if (scanf ("%f", &weight) != 1)
+    fprintf (stderr, SCANFERR);
 
   /* perform calculation */
-  if (metrics == 1)
-  {
-    eibmi((float)weight, (float)height);
-    printf("Your Body mass Index is %f\n", eibmi);
-  }
-  else if (metrics == 2 || metrics == 0x0A)
-  {
-    mbmi((float)weight, (float)height);
-    printf("Your Body mass Index is %f\n", mbmi);
-  }
-  else
-  {
-    printf("invalid choice: choose 1 or 2\n");
-    goto START;
-  }
+  double *bmi = (double *)malloc (sizeof (double));
 
-  printf("This is considered overweight. Time to hit the gym\n");
+  if (metrics == 1)
+    {
+      *bmi = (double)iebmi (weight, height);
+      printf ("Your Body mass Index is %.2f\n", *bmi);
+    }
+  else if (metrics == 2 || metrics == 0x0A)
+    {
+      *bmi = (double)mbmi (weight, height);
+      printf ("Your Body mass Index is %.2f\n", *bmi);
+    }
+  else
+    {
+      printf ("invalid choice: choose 1 or 2\n");
+      goto START;
+    }
+  printf ("You are ");
+  bmi_result (bmi);
+  free (bmi);
 
   return 0;
 }
--- src/bmi.c
+++ src/bmi.c
@@ -1,15 +1,15 @@
-#include <stdio.h>
 #include "bmi.h"
+#include <stdio.h>
 
 /*
  * Imperial English BMI Formula
  * weight (lbs) * 703 / height(in^2)
  */
 float
-eibmi (float weight, float height)
+iebmi (float weight, float height)
 {
-  float result = (weight * (float)703) / (height * height);
-  return result;
+  float imp_result = (weight * 703) / (height * height * 144);
+  return imp_result;
 }
 
 /*
@@ -19,8 +19,8 @@ eibmi (float weight, float height)
 float
 mbmi (float weight, float height)
 {
-  float result = (weight / (height * height));
-  return result;
+  float metrics_result = (weight / ((height / 100) * (height / 100)));
+  return metrics_result;
 }
 
 /*
@@ -28,46 +28,30 @@ mbmi (float weight, float height)
  * returns a recommendation based on your BMI result
  */
 void
-bmi_result (float bmi)
+bmi_result (double *bmi)
 {
-  if (bmi < 15.0)
-    printf ("Very severely underweight\n");
-  else if (bmi >= 15.0 && bmi <= 16.0)
-    printf ("Severely underweight\n");
-  else if (bmi > 16.0 && bmi <= 18.5)
+  if (*bmi < 15.0)
+    printf ("very severely underweight\n");
+  else if (*bmi >= 15.0 && *bmi <= 16.0)
+    printf ("severely underweight\n");
+  else if (*bmi > 16.0 && *bmi <= 18.5)
     printf ("underweight\n");
-  else if (bmi > 18.5 && bmi <= 25.0)
-    printf ("Normal (healthy weight)\n");
-  else if (bmi > 25.0 && bmi <= 30.0)
-    printf ("Overweight\n");
-  else if (bmi > 30.0 && bmi <= 35.0)
-    printf ("Obese Class I (Moderately obese)\n");
-  else if (bmi > 35.0 && bmi <= 40.0)
-    printf ("Obese Class II (Severely obese)\n");
-  else if (bmi > 40.0 && bmi <= 45.0)
-    printf ("Obese Class III (Very severely obese)\n");
-  else if (bmi > 45.0 && bmi <= 50.0)
-    printf ("Obese Class IV (Morbidly obese)\n");
-  else if (bmi > 50.0 && bmi <= 60.0)
-    printf ("Obese Class V (Super obese) \n");
-  else if (bmi > 60.0)
-    printf ("Obese Class VI (Hyper obese)\n");
+  else if (*bmi > 18.5 && *bmi <= 25.0)
+    printf ("normal (healthy weight)\n");
+  else if (*bmi > 25.0 && *bmi <= 30.0)
+    printf ("overweight\n");
+  else if (*bmi > 30.0 && *bmi <= 35.0)
+    printf ("obese Class I (Moderately obese)\n");
+  else if (*bmi > 35.0 && *bmi <= 40.0)
+    printf ("obese Class II (Severely obese)\n");
+  else if (*bmi > 40.0 && *bmi <= 45.0)
+    printf ("obese Class III (Very severely obese)\n");
+  else if (*bmi > 45.0 && *bmi <= 50.0)
+    printf ("obese Class IV (Morbidly obese)\n");
+  else if (*bmi > 50.0 && *bmi <= 60.0)
+    printf ("obese Class V (Super obese) \n");
+  else if (*bmi > 60.0 && *bmi <= 100)
+    printf ("obese Class VI (Hyper obese)\n");
   else
-    printf ("Arre you human?\n");
+    printf (" problably not human.\n");
 }
-/*
-Category				BMI (kg/m2)	BMI Prime
-from	to	from	to
-
-Very severely underweight			15		0.60
-Severely underweight			15	16	0.60	0.64
-Underweight				16	18.5	0.64	0.74
-Normal (healthy weight)			18.5	25	0.74	1.0
-Overweight				25	30	1.0	1.2
-Obese Class I (Moderately obese)	30	35	1.2	1.4
-Obese Class II (Severely obese)		35	40	1.4	1.6
-Obese Class III (Very severely obese)	40	45	1.6	1.8
-Obese Class IV (Morbidly obese)		45	50	1.8	2
-Obese Class V (Super obese)		50	60	2	2.4
-Obese Class VI (Hyper obese)		60		2.4
-*/
